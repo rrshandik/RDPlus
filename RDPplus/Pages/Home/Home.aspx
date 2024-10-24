27b2c1238639152241f1588a3ca433998a457db2
rrshandik <ramashandik@gmail.com>
2024-10-24 13:29:14 +0700

    Add project files.

---
+++ RDPplus/Pages/Home/Home.aspx
@@ -0,0 +1,8 @@
+﻿    <%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="RDPplus.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h1>This should be the main ddd</h1>
+</asp:Content>
+
---
+++ RDPplus/Site1.Master.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RDPplus
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Reset active class for all navigation items
+            navHome.Attributes["class"] = "nav-link";
+            navDataPegawai.Attributes["class"] = "nav-link";
+            navRDP.Attributes["class"] = "nav-link";
+
+            // Determine the current page and set the active class
+            string currentPage = Request.Path.ToLower();
+
+            if (currentPage.Contains("home.aspx"))
+            {
+                navHome.Attributes["class"] += " active";
+            }
+            else if (currentPage.Contains("datapegawai.aspx"))
+            {
+                navDataPegawai.Attributes["class"] += " active";
+            }
+            else if (currentPage.Contains("rdp.aspx"))
+            {
+                navRDP.Attributes["class"] += " active";
+            }
+
+        }
+    }
+}
